dfd947cadc90c562cae34ba6317c2ce02f4bea58
ruhulcse <ruhul.cse7862@gmail.com>
2023-04-04 22:39:50 +0600

    origin added

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ require("dotenv").config();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors({ origin: "https://ai-bakend.onrender.com/" }));
 app.use("/api", auth.authorize);
 app.use(logger("dev"));
 app.use(helmet());
