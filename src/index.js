11ca830fb06d7216fed64e7193021fc3cb8e0fff
ruhulcse <ruhul.cse7862@gmail.com>
2023-04-04 22:44:22 +0600

    origin remove

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ require("dotenv").config();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors({ origin: "https://ai-bakend.onrender.com/" }));
+app.use(cors());
 app.use("/api", auth.authorize);
 app.use(logger("dev"));
 app.use(helmet());
